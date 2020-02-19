368cbf290f19e65631aeaa0686bbb5098c7882b0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-02-19 23:10:52 +0700

    upgrade and improve pages

--- src/components/SubmitMessage.tsx
+++ src/components/SubmitMessage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, useTheme, useMediaQuery } from '@material-ui/core';
+import { Button, useTheme, useMediaQuery, Snackbar } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -11,7 +11,10 @@ import feathersClient from '../feathersClient';
 // @ts-ignore
 const SubmitMessage: any = ({ source, record = {} }) => {
   const [open, setOpen] = React.useState(false);
+  const [openMessage, setOpenMessage] = React.useState(false);
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
   const [value, setValue] = React.useState<string>('');
+  const [message, setMessage] = React.useState<string>('');
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
@@ -23,6 +26,14 @@ const SubmitMessage: any = ({ source, record = {} }) => {
     setOpen(false);
   };
 
+  const handleCloseMessage = () => {
+    setOpenMessage(false);
+  }
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  }
+
   const handleSubmit = () => {
     setOpen(false);
     setValue('');
@@ -31,7 +42,13 @@ const SubmitMessage: any = ({ source, record = {} }) => {
     if (!id || !text || !value) {
       return false;
     }
-    feathersClient.service('message').get(id, { headers: { key: value } });
+    feathersClient.service('message').get(id, { headers: { key: value } }).then(
+      (result: any) => {
+        let message = result.text;
+        setMessage(message);
+        setOpenMessage(true);
+      }
+    ).catch(() => setOpenSnackbar(true));
   }
 
   return (
@@ -63,6 +80,44 @@ const SubmitMessage: any = ({ source, record = {} }) => {
           </Button>
         </DialogActions>
       </Dialog>
+
+      <Dialog
+        fullScreen={fullScreen}
+        open={openMessage}
+        onClose={handleCloseMessage}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"This is your message!"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {message}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseMessage} color="primary" autoFocus>
+            Thanks!
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message="Failed to open message"
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleCloseSnackbar}>
+              OK
+            </Button>
+          </React.Fragment>
+        }
+      />
     </div>
   );
 };
