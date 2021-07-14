3ee3fe96362ba0214731a8b8f4bd28ed53cfc40e
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-07-14 20:31:52 +0700

    Update new dashboard

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,13 +1,69 @@
 // in src/Dashboard.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
+import { Button, Snackbar } from '@material-ui/core';
 import CardContent from '@material-ui/core/CardContent';
 import { Title } from 'react-admin';
-const Dashboard: React.FC = () => (
-  <Card>
-    <Title title="Welcome to the administration" />
-    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-  </Card>
-);
+import feathersClient from './feathersClient';
+
+interface IFeathersData {
+  total: number;
+}
+
+const Dashboard: React.FC = () => {
+  const [totalMessage, setTotalMessage] = useState(0);
+  const [snackBarStatus, setSnackbar] = useState(false);
+
+  const handleCloseSnackbar = () => {
+    setSnackbar(false);
+  };
+
+  useEffect(() => {
+    feathersClient
+      .service('message')
+      .find({
+        query: {
+          $limit: 0,
+        },
+      })
+      .then((result: IFeathersData) => {
+        setTotalMessage(result.total);
+      })
+      .catch((error: Error) => {
+        console.error(error.message);
+        setSnackbar(true);
+      });
+  });
+
+  return (
+    <React.Fragment>
+      <Title title="Welcome to Tell Secret Message for Someone" />
+      <Card>
+        <CardContent>Your total messages: {totalMessage}</CardContent>
+      </Card>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={snackBarStatus}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        message="Failed to open message"
+        action={
+          <React.Fragment>
+            <Button
+              color="secondary"
+              size="small"
+              onClick={handleCloseSnackbar}
+            >
+              OK
+            </Button>
+          </React.Fragment>
+        }
+      />
+    </React.Fragment>
+  );
+};
 
 export default Dashboard;
